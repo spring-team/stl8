442ae4a306f7f8cb89f797d03b618784fd869052
Rod Johnson <johnsonroda@gmail.com>
2018-03-01 16:51:36 -0600

    Initial commit from Atomist

---
+++ src/main/java/atomist/Stl8Application.java
@@ -0,0 +1,12 @@
+package atomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stl8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stl8Application.class, args);
+	}
+}
